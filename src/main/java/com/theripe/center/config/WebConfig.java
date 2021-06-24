bdc65ee3a08fa023257ec9dfa957bffb3b9aba3b
TheRipe <theripe666@163.com>
2021-06-24 10:20:50 +0800

    v4

---
+++ src/main/java/com/theripe/center/config/WebConfig.java
@@ -0,0 +1,11 @@
+package com.theripe.center.config;
+
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @Author TheRipe
+ * @create 2021/6/24 10:19
+ */
+public class WebConfig implements WebMvcConfigurer {
+
+}
