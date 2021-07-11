2417aba9976c297bd9fe57e4f6eb43564abb141d
TheRipe <theripe666@163.com>
2021-07-11 22:53:11 +0800

    v15

--- src/main/java/com/theripe/center/interceptor/AdminLoginInterceptor.java
+++ src/main/java/com/theripe/center/interceptor/AdminLoginInterceptor.java
@@ -16,15 +16,14 @@ import javax.servlet.http.HttpServletResponse;
 public class AdminLoginInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-//       String requestServletPath = request.getServletPath();
-//       if (requestServletPath.startsWith("/admin") && request.getSession().getAttribute("loginUser") == null) {
-//           response.sendRedirect(request.getContextPath()+"/admin/login");
-//           return false;
-//       } else {
-//           request.getSession().removeAttribute("errorMsg");
-//           return true;
-//       }
-        return true;
+       String requestServletPath = request.getServletPath();
+       if (requestServletPath.startsWith("/admin") && request.getSession().getAttribute("loginUser") == null) {
+           response.sendRedirect(request.getContextPath()+"/admin/login");
+           return false;
+       } else {
+           request.getSession().removeAttribute("errorMsg");
+           return true;
+       }
 
     }
 
